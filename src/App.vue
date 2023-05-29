77293c93fbac1d3480221c3af629c76d718423bb
AshOnDiscord <68016221+AshOnDiscord@users.noreply.github.com>
2023-05-29 14:10:35 -0400

    Graphics + En Passant
    Google En Passant. Holy Hell. New Response Just Dropped. Actual Zombie

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,13 @@ const turnOrder: Ref<Team[]> = ref([Team.White, Team.Black]);
 
 const turn: Ref<Team> = ref(turnOrder.value[0]);
 
+const enPassant: Ref<boolean> = ref(false);
+
+const lastMove: Ref<{
+  from: { y: number; x: number };
+  to: { y: number; x: number };
+} | null> = ref(null);
+
 const setupBoard = () => {
   for (let i = 0; i < 8; i++) {
     PieceBoard.value[i] = [];
@@ -137,6 +144,26 @@ const selectPiece = (y: number, x: number) => {
       ) {
         viewBoard.value[y + 1][x - 1] = true;
       }
+      // en passant
+      const last = lastMove.value;
+      if (last) {
+        const lastPiece = PieceBoard.value[last.to.y][last.to.x];
+        const distance = Math.abs(last.to.y - last.from.y);
+        if (
+          lastPiece.team !== Team.White &&
+          lastPiece.piece == Piece.Pawn &&
+          distance == 2 &&
+          y == last.to.y
+        ) {
+          if (last.to.x == x + 1) {
+            viewBoard.value[y + 1][x + 1] = true;
+            enPassant.value = true;
+          } else if (last.to.x == x - 1) {
+            viewBoard.value[y + 1][x - 1] = true;
+            enPassant.value = true;
+          }
+        }
+      }
     } else {
       if (
         canMove(y - 1, x, Team.Black) &&
@@ -162,6 +189,26 @@ const selectPiece = (y: number, x: number) => {
         PieceBoard.value[y - 1][x - 1].team !== null
       ) {
         viewBoard.value[y - 1][x - 1] = true;
+      }
+    }
+    // en passant
+    const last = lastMove.value;
+    if (last) {
+      const lastPiece = PieceBoard.value[last.to.y][last.to.x];
+      const distance = Math.abs(last.to.y - last.from.y);
+      if (
+        lastPiece.team !== Team.Black &&
+        lastPiece.piece == Piece.Pawn &&
+        distance == 2 &&
+        y == last.to.y
+      ) {
+        if (last.to.x == x + 1) {
+          viewBoard.value[y - 1][x + 1] = true;
+          enPassant.value = true;
+        } else if (last.to.x == x - 1) {
+          viewBoard.value[y - 1][x - 1] = true;
+          enPassant.value = true;
+        }
       }
     }
   } else if (square.piece == Piece.Knight) {
@@ -301,6 +348,17 @@ const selectPiece = (y: number, x: number) => {
 const movePiece = (y: number, x: number) => {
   if (!selectedPiece.value) return;
 
+  const lastCopy = {
+    from: {
+      x: selectedPiece.value.x,
+      y: selectedPiece.value.y,
+    },
+    to: {
+      x: x,
+      y: y,
+    },
+  };
+
   const pieceCopy =
     PieceBoard.value[selectedPiece.value.y][selectedPiece.value.x];
 
@@ -311,60 +369,163 @@ const movePiece = (y: number, x: number) => {
 
   PieceBoard.value[y][x] = pieceCopy;
 
+  if (enPassant.value) {
+    if (lastMove.value?.to.x == x) {
+      if (turn.value == Team.White && lastMove.value?.to.y == y - 1) {
+        PieceBoard.value[y - 1][x] = {
+          piece: null,
+          team: null,
+        };
+      } else if (turn.value == Team.Black && lastMove.value?.to.y == y + 1) {
+        PieceBoard.value[y + 1][x] = {
+          piece: null,
+          team: null,
+        };
+      }
+    }
+  }
+
   selectedPiece.value = null;
   resetViewBoard();
 
   const turnIndex = turnOrder.value.indexOf(turn.value);
   turn.value = turnOrder.value[(turnIndex + 1) % turnOrder.value.length];
+
+  lastMove.value = lastCopy;
+};
+
+const getIcon = (piece: Piece, team: Team | null) => {
+  if (piece === null) return "";
+  let url = "https://www.chess.com/chess-themes/pieces/neo/150/";
+  if (team == Team.White) url += "w";
+  if (team == Team.Black) url += "b";
+
+  switch (piece) {
+    case Piece.Pawn:
+      url += "p";
+      break;
+    case Piece.Knight:
+      url += "n";
+      break;
+    case Piece.Bishop:
+      url += "b";
+      break;
+    case Piece.Rook:
+      url += "r";
+      break;
+    case Piece.Queen:
+      url += "q";
+      break;
+    case Piece.King:
+      url += "k";
+      break;
+  }
+  return url + ".png";
 };
 </script>
 
 <template>
-  {{ turn }}
-  <main class="grid grid-rows-[repeat(8,2rem)]">
+  <!-- {{ turn }} -->
+  <div class="grid grid-cols-2">
     <div
-      v-for="(row, i) in PieceBoard.slice().reverse()"
-      :key="i"
-      class="grid grid-cols-[repeat(8,2rem)]"
+      class="bg-[hsl(60,36%,80%)] flex justify-end gap-16 min-h-screen items-center pr-8"
     >
-      <div
-        v-for="(column, j) in row"
-        :key="j"
-        class="flex justify-center items-center"
-        :class="{
-          'bg-amber-600': (i + j) % 2 == 1,
-          'bg-orange-200': (i + j) % 2 == 0,
-          '!bg-green-500': viewBoard[7 - i][j],
-        }"
-        @click="
-          viewBoard[7 - i][j] ? movePiece(7 - i, j) : selectPiece(7 - i, j)
-        "
-      >
-        {{ column.piece == Piece.King && column.team == Team.White ? "♔" : "" }}
-        {{
-          column.piece == Piece.Queen && column.team == Team.White ? "♕" : ""
-        }}
-        {{
-          column.piece == Piece.Bishop && column.team == Team.White ? "♗" : ""
-        }}
-        {{
-          column.piece == Piece.Knight && column.team == Team.White ? "♘" : ""
-        }}
-        {{ column.piece == Piece.Rook && column.team == Team.White ? "♖" : "" }}
-        {{ column.piece == Piece.Pawn && column.team == Team.White ? "♙" : "" }}
-        {{ column.piece == Piece.King && column.team == Team.Black ? "♚" : "" }}
-        {{
-          column.piece == Piece.Queen && column.team == Team.Black ? "♛" : ""
-        }}
-        {{
-          column.piece == Piece.Bishop && column.team == Team.Black ? "♝" : ""
-        }}
-        {{
-          column.piece == Piece.Knight && column.team == Team.Black ? "♞" : ""
-        }}
-        {{ column.piece == Piece.Rook && column.team == Team.Black ? "♜" : "" }}
-        {{ column.piece == Piece.Pawn && column.team == Team.Black ? "♟" : "" }}
-      </div>
+      <main class="grid grid-rows-[repeat(8,2rem)]">
+        <div
+          v-for="(row, i) in PieceBoard.slice().reverse()"
+          :key="i"
+          class="grid grid-cols-[repeat(8,2rem)]"
+        >
+          <div
+            v-for="(column, j) in row"
+            :key="j"
+            class="flex justify-center items-center relative"
+            :class="{
+              'bg-[#4b7299]': (i + j) % 2 == 1,
+              'bg-[#eaead2]': (i + j) % 2 == 0,
+            }"
+            @click="
+              viewBoard[7 - i][j] ? movePiece(7 - i, j) : selectPiece(7 - i, j)
+            "
+          >
+            <div
+              class="w-8 h-8 absolute top-0 left-0 flex justify-center items-center z-100"
+              v-if="viewBoard[7 - i][j]"
+            >
+              <div
+                class="w-3 h-3 bg-black/10 rounded-full"
+                v-if="PieceBoard[7 - i][j].piece === null"
+              ></div>
+              <div
+                v-else
+                class="w-8 h-8 border-4 border-black/10 rounded-full"
+              ></div>
+            </div>
+            <div
+              class="absolute w-8 h-8 top-0 left-0 bg-[#00a5ff] opacity-50"
+              v-if="
+                lastMove !== null &&
+                ((lastMove.to.y === 7 - i && lastMove.to.x === j) ||
+                  (lastMove?.from.y === 7 - i && lastMove?.from.x === j))
+              "
+            ></div>
+            <img
+              v-if="column.piece !== null"
+              :src="getIcon(column.piece, column.team)"
+              class="pointer-events-none relative z-10"
+            />
+          </div>
+        </div>
+      </main>
     </div>
-  </main>
+    <div
+      class="bg-[hsl(210,34%,50%)] flex justify-start gap-16 min-h-screen items-center pl-8"
+    >
+      <main class="grid grid-rows-[repeat(8,2rem)]">
+        <div
+          v-for="(row, i) in PieceBoard.slice()"
+          :key="i"
+          class="grid grid-cols-[repeat(8,2rem)]"
+        >
+          <div
+            v-for="(column, j) in row"
+            :key="j"
+            class="flex justify-center items-center relative"
+            :class="{
+              'bg-[#4b7299]': (i + j) % 2 == 1,
+              'bg-[#eaead2]': (i + j) % 2 == 0,
+            }"
+            @click="viewBoard[i][j] ? movePiece(i, j) : selectPiece(i, j)"
+          >
+            <div
+              class="w-8 h-8 absolute top-0 left-0 flex justify-center items-center"
+              v-if="viewBoard[i][j]"
+            >
+              <div
+                class="w-3 h-3 bg-black/10 rounded-full"
+                v-if="PieceBoard[i][j].piece === null"
+              ></div>
+              <div
+                v-else
+                class="w-8 h-8 border-4 border-black/10 rounded-full"
+              ></div>
+            </div>
+            <div
+              class="absolute w-8 h-8 top-0 left-0 bg-[#00a5ff] opacity-50"
+              v-if="
+                lastMove !== null &&
+                ((lastMove.to.y === i && lastMove.to.x === j) ||
+                  (lastMove?.from.y === i && lastMove?.from.x === j))
+              "
+            ></div>
+            <img
+              v-if="column.piece !== null"
+              :src="getIcon(column.piece, column.team)"
+              class="pointer-events-none relative z-10"
+            />
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
